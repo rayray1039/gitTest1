4bd5391cf10317d4c30f438991f34da75bfd4c93
rayray1039 <raymonddelia@Raymonds-iMac.local>
2013-08-22 22:41:57 -0700

    box controller update
    box controller will not make random boxes if a param is not fed in.

--- boxController.lua
+++ boxController.lua
@@ -9,8 +9,16 @@ function boxController:new(params)
     --{x=10,y=10,w=10,h=10,color = {r=255,g=0,b=0,a=255}}
     local settings = params or {}
     local myBox ={}
-    myBox.DO = display.newRect(settings.x, settings.y, settings.w, settings.h)
-    myBox.DO:setFillColor(settings.color.r,settings.color.g,settings.color.b,settings.color.a)
+    
+    local size = math.random(5, 20)
+    local x = settings.x or math.random( 0, display.contentWidth  - size /2)
+    local y = settings.y or math.random( 0, display.contentHeight  - size /2)
+    local w = settings.w or size
+    local h = settings.h or size
+    local myColor = settings.color or {r=math.random(128,255),g=math.random(128,255),b=math.random(128,255),a=255}
+    
+    myBox.DO = display.newRect(x, y, w, h)
+    myBox.DO:setFillColor(myColor.r,myColor.g,myColor.b,myColor.a)
     
     return myBox
     
--- main.lua
+++ main.lua
@@ -14,7 +14,14 @@ settings = {x=10,y=10,w=10,h=10,color = {r=255,g=0,b=0,a=255}}
 local myBox1= myBoxController:new(settings)
 
 
-settings2 = {x=50,y=10,w=10,h=10,color = {r=255,g=255,b=0,a=255}}
+local settings2 = {x=50,y=10,w=10,h=10,color = {r=255,g=255,b=0,a=255}}
 
-local myBox1= myBoxController:new(settings2)
+local myBox2= myBoxController:new(settings2)
+
+
+for i = 1 , 20 do
+   local myBox= myBoxController:new( )
+end
+
+
 
