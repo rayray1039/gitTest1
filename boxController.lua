c6fbf3421b83098b4283301a5394c7bd1f847aab
rayray1039 <raymonddelia@Raymonds-iMac.local>
2013-08-22 22:31:36 -0700

    Commit #1
    changed boxcontroller to accept parameters, and draw a second box in
    main

--- boxController.lua
+++ boxController.lua
@@ -6,11 +6,11 @@
 local boxController = {}
 
 function boxController:new(params)
-    
-    local settings = {} or params
+    --{x=10,y=10,w=10,h=10,color = {r=255,g=0,b=0,a=255}}
+    local settings = params or {}
     local myBox ={}
-    myBox.DO = display.newRect(10, 10, 10, 10)
-    myBox.DO:setFillColor(0,255,255, 255)
+    myBox.DO = display.newRect(settings.x, settings.y, settings.w, settings.h)
+    myBox.DO:setFillColor(settings.color.r,settings.color.g,settings.color.b,settings.color.a)
     
     return myBox
     
--- main.lua
+++ main.lua
@@ -10,5 +10,11 @@
 
 local myBoxController = require("boxController")
 
+settings = {x=10,y=10,w=10,h=10,color = {r=255,g=0,b=0,a=255}}
+local myBox1= myBoxController:new(settings)
 
-local myBox1= myBoxController:new()
+
+settings2 = {x=50,y=10,w=10,h=10,color = {r=255,g=255,b=0,a=255}}
+
+local myBox1= myBoxController:new(settings2)
+
